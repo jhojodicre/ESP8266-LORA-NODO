d125a0c86a7698335dcfc20ebcdbb4945181fead
Jhonny Diaz <jhojodicre@gmial.com>
2023-04-24 16:43:14 -0300

    Renombrar Variables del Protocoo RFM95

--- Esp8266_Lora_Esclavo.ino
+++ Esp8266_Lora_Esclavo.ino
@@ -66,7 +66,8 @@
     // Variables para enviar.
     int packetSize = 0;
     String outgoing;              // outgoing message
-    byte msgCount = 0;            // count of outgoing messages
+    byte msgCount1 = 0;            // count of outgoing messages
+    byte msgCount2 = 0;            // count of outgoing messages
     byte localAddress = 0x01;     // address of this device           a3
     byte destination = 0xFF;      // destination to send to           a4
     long lastSendTime = 0;        // last send time
@@ -247,7 +248,7 @@ void loop(){
       destination = direccion_aux;
     }
     void a5_Nodo_Mensaje_ID(){      
-      msgCount++;                           // increment message ID.
+      msgCount1++;                           // increment message ID.
     }
     void a6_Nodo_Numeros(int parametro_1){
       Nodos=parametro_1;
@@ -417,7 +418,8 @@ void loop(){
       LoRa.beginPacket();                   // start packet
       LoRa.write(destination);              // add destination address
       LoRa.write(localAddress);             // add sender address
-      LoRa.write(msgCount);                 // add message ID
+      LoRa.write(msgCount1);                 // add message ID
+      LoRa.write(msgCount2);                 // add message ID
       LoRa.write(outgoing.length());        // add payload length
       LoRa.print(outgoing);                 // add payload
       LoRa.endPacket();                     // finish packet and send it
